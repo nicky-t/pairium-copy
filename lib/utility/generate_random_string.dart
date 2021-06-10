19f0736a6bd522d14c9d3bff9905c46e650e684a
nicky-t <nickytaketo0903@gmail.com>
2021-06-11 01:34:23 +0900

    add: utility ファイル追加

---
+++ lib/utility/generate_random_string.dart
@@ -0,0 +1,17 @@
+import 'dart:math';
+
+String generateRandomString(int length) {
+  const _randomChars =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const _charsLength = _randomChars.length;
+
+  final rand = Random();
+  final codeUnits = List.generate(
+    length,
+    (index) {
+      final n = rand.nextInt(_charsLength);
+      return _randomChars.codeUnitAt(n);
+    },
+  );
+  return String.fromCharCodes(codeUnits);
+}
